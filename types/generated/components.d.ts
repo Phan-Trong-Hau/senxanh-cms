39b63d0bbf82f4f4795c40a99d923a169e7940ef
PhanTrongHau <haupk2003@gmail.com>
2025-03-09 22:17:11 +0700

    feat: add homepage components and API structure for About Us and Hero Banner

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,55 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface CommonCta extends Struct.ComponentSchema {
+  collectionName: 'components_common_ctas';
+  info: {
+    displayName: 'CTA';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageAboutUs extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_about_uses';
+  info: {
+    displayName: 'About Us';
+  };
+  attributes: {
+    description: Schema.Attribute.RichText;
+    label: Schema.Attribute.String;
+    thumbnail: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageHeroBanner extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_hero_banners';
+  info: {
+    description: '';
+    displayName: 'Hero Banner';
+  };
+  attributes: {
+    background: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    backgroundInMobile: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    description: Schema.Attribute.Text;
+    highlightTitle: Schema.Attribute.String;
+    logo: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'common.cta': CommonCta;
+      'homepage.about-us': HomepageAboutUs;
+      'homepage.hero-banner': HomepageHeroBanner;
+    }
+  }
+}
