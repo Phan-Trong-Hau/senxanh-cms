0c484c760a6c5f4f448990612d06dd8d9dc49f7f
PhanTrongHau <haupk2003@gmail.com>
2025-03-09 18:43:01 +0700

    feat: update server configuration for AWS S3 integration and change default port

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,34 @@
-export default [
-  'strapi::logger',
+export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
