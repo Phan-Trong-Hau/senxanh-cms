5e44c3ff8b9d4dd2f94ca831b0a16366b4c49c3b
PhanTrongHau <haupk2003@gmail.com>
2025-11-15 16:38:28 +0700

    feat: add encryptedKey attribute to AdminApiToken schema and update webp-converter configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -28,4 +28,14 @@ export default ({ env }) => ({
       defaultDepth: 5,
     },
   },
+  'webp-converter': {
+    enabled: true,
+    config: {
+      // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
+      mimeTypes: undefined,
+      options: {
+        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+      },
+    },
+  },
 });
